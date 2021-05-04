007fc38f3017a4007372193d9b54940941b304fe
mairitsaal <mairit.saal@khk.ee>
2021-05-04 10:37:29 +0300

    Creating the API Client

---
+++ opinion-ate/src/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL:
+    'https://outside-in-dev-api.herokuapp.com/r9NhvoT5T0I4Zn3V74AKgtk3q5CLkI06',
+});
+
+const api = {
+  loadRestaurants() {
+    return client.get('/restaurants').then(response => response.data);
+  },
+};
+
+export default api;
