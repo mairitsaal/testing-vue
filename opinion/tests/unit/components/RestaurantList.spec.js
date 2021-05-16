cafd023b4e5e0e76d125511d526191ce4880097a
mairitsaal <mairit.saal@khk.ee>
2021-05-16 17:26:28 +0300

    Unit Testing the Store

--- opinion/tests/unit/components/RestaurantList.spec.js
+++ opinion/tests/unit/components/RestaurantList.spec.js
@@ -54,17 +54,18 @@ describe('RestaurantList', () => {
     beforeEach(() => {
           mountWithStore();
         });
+
     it('does not display the loading indicator while not loading', () => {
       expect(wrapper.find('[data-testid="loading-indicator"]').exists()).toBe(false);
-    });
-
-    it('does not display the error message', () => {
-      expect(wrapper.find('[data-testid="loading-error"]').exists()).toBe(false,);
     });
 
     it('displays the restaurants', () => {
       expect(findByTestId(wrapper, 'restaurant', 0).text()).toBe('Sushi Place');
       expect(findByTestId(wrapper, 'restaurant', 1).text()).toBe('Pizza Place');
+    });
+
+    it('does not display the error message', () => {
+      expect(wrapper.find('[data-testid="loading-error"]').exists()).toBe(false);
     });
   });
 
