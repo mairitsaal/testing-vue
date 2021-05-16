e15785be880055879672b10a4ed40da0cef21b67
mairitsaal <mairit.saal@khk.ee>
2021-05-16 18:16:43 +0300

    Add Restaurants

--- opinion/src/components/NewRestaurantForm.vue
+++ opinion/src/components/NewRestaurantForm.vue
@@ -1,20 +1,41 @@
 <template>
   <form @submit.prevent="handleSave">
-    <v-text-field
-        placeholder="Add Restaurant"
-        filled
-        type="text"
-        v-model="name"
-        data-testid="new-restaurant-name-field"
-    />
-    <v-btn
-        type="submit"
-        color="primary"
-        class="black--text"
-        data-testid="new-restaurant-submit-button"
+    <v-alert
+        v-if="validationError"
+        type="error"
+        data-testid="new-restaurant-name-error"
     >
-      Add
-    </v-btn>
+      Name is required.
+    </v-alert>
+    <v-alert
+        v-if="serverError"
+        type="error"
+        data-testid="new-restaurant-server-error"
+    >
+      The restaurant could not be saved. Please try again.
+    </v-alert>
+    <v-row>
+      <v-col cols="9">
+        <v-text-field
+            placeholder="Add Restaurant"
+            filled
+            type="text"
+            v-model="name"
+            data-testid="new-restaurant-name-field"
+        />
+      </v-col>
+      <v-col cols="3">
+        <v-btn
+            type="submit"
+            color="primary"
+            class="black--text"
+            block
+            data-testid="new-restaurant-submit-button"
+        >
+          Add
+        </v-btn>
+      </v-col>
+    </v-row>
   </form>
 </template>
 
@@ -26,6 +47,8 @@ export default {
   data() {
     return {
       name: '',
+      validationError: false,
+      serverError: false,
     };
   },
   methods: {
@@ -33,7 +56,20 @@ export default {
       createRestaurant: 'restaurants/create',
     }),
     handleSave() {
-      this.createRestaurant(this.name);
+      if (this.name) {
+        this.validationError = false;
+        this.serverError = false;
+
+        this.createRestaurant(this.name)
+            .then(() => {
+              this.name = '';
+            })
+            .catch(() => {
+              this.serverError = true;
+            });
+      } else {
+        this.validationError = true;
+      }
     },
   },
 };
